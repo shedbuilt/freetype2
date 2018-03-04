f287377572c77cb1d8b71676cf5e92e388b8d7ea
Auston Stewart <auston@shedbuilt.net>
2018-03-04 12:12:03 -1000

    Initial commit of freetype2

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg && \
+sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
+    -i include/freetype/config/ftoption.h  && \
+./configure --prefix=/usr \
+            --disable-static && \
+make -j $SHED_NUMJOBS && \
+make DESTDIR="$SHED_FAKEROOT" install
