b3000e13deb148c462be85e564813f1a2cbbb84f
Auston Stewart <auston@shedbuilt.net>
2018-10-25 21:16:16 -1000

    Updating to v2.9.1

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg && \
+# Patch per BLFS to enable GX/AAT and OpenType table validation as well as Subpixel Rendering
+sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg &&
 sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
-    -i include/freetype/config/ftoption.h  && \
+    -i include/freetype/config/ftoption.h  &&
+
+# Configure
 ./configure --prefix=/usr \
-            --disable-static && \
-make -j $SHED_NUM_JOBS && \
-make DESTDIR="$SHED_FAKE_ROOT" install
+            --enable-freetype-config \
+            --disable-static &&
+
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+install -Dm755 builds/unix/freetype-config "${SHED_FAKE_ROOT}/usr/bin/freetype-config"
